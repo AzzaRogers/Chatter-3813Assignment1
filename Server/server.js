7aa52e00e31b320cfd68cf441eceeb4a45e1bcb6
Aaron Rogers <aaron@itsquires.com>
2020-09-09 12:31:19 +1000

    Added more server functionality

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,33 @@
-var express = require("express"); //used for routing
+
+
+var express = require("express"); 
 var app = express();
-var http = require("http").Server(app); //used to provide http functionality
+var http = require("http").Server(app); 
+var bodyParser = require("body-parser");
+var cors = require("cors") //import the cors package.
 
-app.use(express.static(__dirname + "/www"));
+app.use(cors()); // Add cors middleware to the express application
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../dist/Chatter/"));
 
+// Start server, listen to port 3000 and log host and port
 let server = http.listen(3000, function () {
     let host = server.address().address;
     let port = server.address().port;
-    console.log("My First Nodejs Server!");
-    console.log("Server listening on: " + host + " port: " + port);
+    var date = new Date();
+    console.log("Server listening on: " + host + " port: " + port + " At: " + date);
 });
 
 
 app.get("/test", function (req, res) {
-        res.sendFile(__dirname + "/www/index.html");
-        });
+    console.log("/test");
+    if (!req.body) {
+        return res.sendStatus(400);        
+    }
+
+    var user = {}
+    user.email = req.body.email;
+    user.upwd = req.body.upwd;
+    
+    res.send(user);
+});
